49639c6abbdf80bed3c95aac9a9374987ac5b528
Andre Fonseca <oandrefonseca@gmail.com>
2024-05-01 13:50:11 -0500

    Changing lauchDir to baseDir

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -5,20 +5,12 @@ import pandas as pd
 
 def setup_input_parameters(ds: PreprocessDataset):
 
-    # If the user did not select a custom Cell Markers DB CSV, use the default
-    if ds.params.get("annotation_db") is None:
-        ds.add_param(
-            "annotation_db",
-            "./assets/cell_markers_database.csv"
-        )
-
     # If the user did not select a custom Meta-Program CSV, use the default
     if ds.params.get("input_cell_mask") is None:
         ds.add_param(
             "input_cell_mask",
             "./assets/NO_FILE"
         )
-
 
 if __name__ == "__main__":
 
