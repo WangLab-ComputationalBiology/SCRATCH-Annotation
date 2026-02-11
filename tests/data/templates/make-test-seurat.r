5a59fabe395254cd32c5935ed1276693c129c481
dimalvovs <dmitrijs.lvovs@gmail.com>
2026-02-11 11:27:22 -0500

    increase genes to 2500 to avoid AddModuleScore() nbin error

--- tests/data/templates/make-test-seurat.r
+++ tests/data/templates/make-test-seurat.r
@@ -9,13 +9,20 @@ set.seed(42)
 cmdb <- read.csv("${cell_markers_database}", stringsAsFactors = FALSE)
 
 
-#create a tiny Seurat object for testing purposes
-n_genes <- 200
+# create a tiny Seurat object for testing purposes
+# below 2500 Seurat AddModuleScore fails with error described below
+# https://github.com/satijalab/seurat/issues/4819#issuecomment-2825615354
+n_markers <- 200
+n_genes <- 2500
 n_cells <- 1000
-genes <- sample(unique(cmdb[["markers"]]), n_genes, replace = FALSE)
+genes <- sample(unique(cmdb[["markers"]]), n_markers, replace = FALSE)
+
+# add some dummy genes to please Seurat's AddModuleScore and pipeline
+genes <- c(genes, paste0("TEST", 1:(n_genes - length(genes))))
+
 sr <- CreateSeuratObject(
   counts = matrix(
-    data = rpois(n = n_genes * n_cells, lambda = 10),
+    data = rnbinom(n_genes * n_cells, size = 1, mu = 100),
     nrow = n_genes,
     ncol = n_cells,
     dimnames = list(
@@ -30,7 +37,7 @@ sr <- FindVariableFeatures(sr)
 sr <- ScaleData(sr, features = VariableFeatures(sr))
 sr <- RunPCA(sr, features = VariableFeatures(sr))
 
-#sctype wants seurat_clusters in query
+# sctype module wants seurat_clusters in query
 sr[["seurat_clusters"]] <- sample(
   x = c("TypeA", "TypeB", "TypeC"),
   size = n_cells,
@@ -39,11 +46,11 @@ sr[["seurat_clusters"]] <- sample(
 
 saveRDS(sr, file = "sr_tiny.rds")
 
-#create a tiny reference Seurat object with seurat_annotation column for azimuth anotation
+# a tiny reference Seurat object with seurat_annotation column for azimuth
 n_cells_ref <- 1000
 sr_ref <- CreateSeuratObject(
   counts = matrix(
-    data = rpois(n = n_genes * n_cells_ref, lambda = 10),
+    data = rnbinom(n_genes * n_cells_ref, size = 1, mu = 100),
     nrow = n_genes,
     ncol = n_cells_ref,
     dimnames = list(
@@ -53,17 +60,18 @@ sr_ref <- CreateSeuratObject(
   )
 )
 
-#azimuth wants seurat annotations in ref
+# azimuth wants seurat annotations in ref
 sr_ref[["seurat_annotations"]] <- sample(
   x = c("TypeA", "TypeB", "TypeC"),
   size = n_cells_ref,
   replace = TRUE
 )
 
-
 sr_ref <- NormalizeData(sr_ref)
 sr_ref <- FindVariableFeatures(sr_ref)
 sr_ref <- ScaleData(sr_ref, features = VariableFeatures(sr_ref))
+
+# azimuth also wants PCA in ref
 sr_ref <- RunPCA(sr_ref, features = VariableFeatures(sr_ref))
 
 saveRDS(sr_ref, file = "sr_ref_tiny.rds")
